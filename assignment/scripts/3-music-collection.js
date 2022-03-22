1a33f3876660c070a017af18370f8c4cade725da
Christopher Boyd <boyd.christopher.t@gmail.com>
2022-03-22 16:36:28 -0500

    created the addToCollection function and successfully added an album

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,14 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+    let album = new Object();
+    album.title = title;
+    album.artist = artist;
+    album.yearPublished = yearPublished;
+    collection.push(album);
+    return album;
+}
+
+addToCollection('Surfer Rosa', 'Pixies', 1988);
