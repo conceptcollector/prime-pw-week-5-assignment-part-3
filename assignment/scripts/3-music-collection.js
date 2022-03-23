2fc14bc5695d906b7b65271a8d3359c9630a0c00
Christopher Boyd <boyd.christopher.t@gmail.com>
2022-03-23 18:56:14 -0500

    To give myself the chance to test my code a bit more, I broadened the function to accept more than just one array and created a couple alternatives

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -4,36 +4,52 @@ let collection = [];
 
 let album;
 
-function addToCollection(title, artist, yearPublished) {
-    collection.push(album = {
-        Title: title,
-        Artist: artist,
-        'Year Published': yearPublished
+function addToCollection(recordCollection, title, artist, yearPublished) {
+    recordCollection.push(album = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
         }
     )
     return album;
 }
 
-addToCollection('Blackstar', 'David Bowie', 2016);
+addToCollection(collection, 'Blackstar', 'David Bowie', 2016);
 console.log(album);
-addToCollection('Let It Be', 'The Beatles', 1970);
+addToCollection(collection, 'Let It Be', 'The Beatles', 1970);
 console.log(album);
-addToCollection('Let It Be', 'The Replacements', 1984);
+addToCollection(collection, 'Let It Be', 'The Replacements', 1984);
 console.log(album);
-addToCollection('Alice', 'Tom Waits', 2002);
+addToCollection(collection, 'Alice', 'Tom Waits', 2002);
 console.log(album);
-addToCollection('Blood Money', 'Tom Waits', 2002);
+addToCollection(collection, 'Blood Money', 'Tom Waits', 2002);
 console.log(album);
-addToCollection('Beyonce', 'Beyonce', 2013);
+addToCollection(collection, 'Beyonce', 'Beyonce', 2013);
 console.log(album);
 console.log(collection);
 
+// After office hours today I thought testing my functions might be a good idea.
+//Broadened addToCollection to work with more than just the one array
+let verifiableCollection = [];
+const emptyCollection = [];
+
+addToCollection(verifiableCollection, 'Blue', 'Joni Mitchell', 1971);
+addToCollection(verifiableCollection, 'The Miseducation of Lauryn Hill', 'Lauryn Hill', 1998);
+addToCollection(verifiableCollection, 'Fetch the Bolt Cutters', 'Fiona Apple', 2020);
+addToCollection(verifiableCollection, 'Blacks Magic', 'Salt-N-Pepa', 1990);
+addToCollection(verifiableCollection, 'Pastel Blues', 'Nina Simone', 1965);
+addToCollection(verifiableCollection, 'Zen Arcade', 'Hüsker Dü', 1984);
+
 function showCollection(array) {
     console.log(`My collection currently holds ${array.length} albums.`);
-    for (item of array) {
-        console.log(item);
+    for (let item of array) {
+        console.log(`${item.title} by ${item.artist}, published in ${item.yearPublished}.`);
     }
     return;
 }
 
 showCollection(collection);
+showCollection(verifiableCollection);
+showCollection(emptyCollection);
+
+//It did what I thought it should!
