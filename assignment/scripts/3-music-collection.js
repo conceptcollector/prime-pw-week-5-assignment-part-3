5e1957dd64a5325a0253f914379e29b8ea2b3e61
Christopher Boyd <boyd.christopher.t@gmail.com>
2022-03-23 13:42:44 -0500

    False celebration yesterday but I got it now

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,13 +2,28 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
+let album;
+
 function addToCollection(title, artist, yearPublished) {
-    let album = new Object();
-    album.title = title;
-    album.artist = artist;
-    album.yearPublished = yearPublished;
-    collection.push(album);
-    return album;
+    collection.push(album = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+        }
+    )
+    return this.album;
 }
 
-addToCollection('Surfer Rosa', 'Pixies', 1988);
+addToCollection('Blackstar', 'David Bowie', 2016);
+console.log(album);
+addToCollection('Let It Be', 'The Beatles', 1970);
+console.log(album);
+addToCollection('Let It Be', 'The Replacements', 1984);
+console.log(album);
+addToCollection('Alice', 'Tom Waits', 2002);
+console.log(album);
+addToCollection('Blood Money', 'Tom Waits', 2002);
+console.log(album);
+addToCollection('Beyonce', 'Beyonce', 2013);
+console.log(album);
+console.log(collection);
